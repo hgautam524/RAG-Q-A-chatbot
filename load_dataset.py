2292776939015f73ccb448b79d71ced310cb2eb8
hgautam524 <hgautam524@gmail.com>
2025-07-13 12:00:30 +0530

    Create load_dataset.py

---
+++ load_dataset.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+df = pd.read_csv("Training Dataset.csv")
+df.fillna("Unknown", inplace=True)
+
+# Create doc-style text for each row
+documents = []
+for i, row in df.iterrows():
+    doc = f"Applicant: {row['Gender']}, {row['Married']}, {row['Education']}, {row['Self_Employed']}.\n"
+    doc += f"Income: {row['ApplicantIncome']}, Loan Amount: {row['LoanAmount']}, Term: {row['Loan_Amount_Term']}.\n"
+    doc += f"Credit History: {row['Credit_History']}, Status: {row['Loan_Status']}"
+    documents.append(doc)
